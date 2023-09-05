2e4e56f8ee1aeb5752444501bf86a753d9017013
daceyforward <dacey.loving@gmail.com>
2023-09-04 23:29:19 -0400

    finished project

--- controllers/goalControllers.js
+++ controllers/goalControllers.js
@@ -14,8 +14,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Goal.find({})
         .then(goals => {
-            console.log('These are the goals', goals)
-
+            //console.log('These are the goals', goals)
             res.render('goals/index', { goals, title: 'All Goals' })
         })
         .catch(error => console.error)
@@ -24,9 +23,7 @@ router.get('/', (req, res) => {
 // CREATE
 
 router.post('/', checkLogin, (req, res) => { 
-
     req.body.owner = req.user._id
-
     console.log(req.body)
     Goal.create(req.body)
         .then(goal => {
@@ -50,7 +47,7 @@ router.patch('/edit/:id', checkLogin, (req, res) => {
             }
         })
         .then(data => {
-            console.log('what is returned from updateOne', data)
+            //console.log('what is returned from updateOne', data)
 
             res.redirect('/goals')
         })
@@ -78,7 +75,7 @@ router.patch('/edit/:id', checkLogin, (req, res) => {
 // NEW
 
 router.get('/new', checkLogin, (req, res) => { 
-    console.log('This is where you can add a new goal.')
+    //console.log('This is where you can add a new goal.')
     res.render('goals/new', { title: 'Add a New Goal'})
 })
 
@@ -87,8 +84,7 @@ router.get('/new', checkLogin, (req, res) => {
 router.get('/edit/:id', checkLogin, (req, res) => { 
     Goal.findById(req.params.id)
         .then(goal => {
-            console.log('Here is one goal to edit', goal)
-
+            //console.log('Here is one goal to edit', goal)
             res.render('goals/edit', { goal, title: 'Edit Goal'})
         })
         .catch(error => console.error)
@@ -96,57 +92,16 @@ router.get('/edit/:id', checkLogin, (req, res) => {
 
 // SHOW
 
-// router.get('/:id', (req, res) => {
-//     Goal.findById(req.params.id)
-//         .then(goal => {
-//             console.log('Here is one goal', goal)
-
-//             // res.render("goals/show", { goal, title:`${goal.desc}`})
-//             res.render("goals/show", { goal, title: 'Goal:'})
-//         })
-//         .catch(error => console.error)
-
-//     // Objective.findById(req.params.id)
-//     //     .then(objective => {
-//     //         console.log('Here is one obj', objective)
-
-//     //         // res.render("goals/show", { goal, title:`${goal.desc}`})
-//     //         res.render("goals/show", { objective, title: 'Objectives:'})
-//     //     })
-//     //     .catch(error => console.error)
-    
-// })
-
 router.get('/:id', (req, res) => {
     Goal.findById(req.params.id)
         .populate('owner')
         .populate('objectives.author')
         .then(goal => {
-            console.log('Here is a goal', goal)
+            //console.log('Here is a goal', goal)
             res.render('goals/show', { goal, title: 'Goal'})
         })
         .catch(error => console.error)
 })
-
-// router.post('/goals/:id', checkLogin, (req, res) => {
-
-//     req.body.author = req.user._id
-
-//     Goal.findById(req.params.goalId)
-
-//         .then(goal => {
-//             goal.objectives.push(req.body)
-//             res.render(goal.save())
-
-//             // res.render(`/goals/${goal._id}/objectives`)
-//         })
-
-//         .then(goal => {
-//             res.redirect(`/goals/${goal._id}`)
-//         })
-
-//         .catch(error => console.error)
-// })
 
 // ------- EXPORT ROUTER ------- \\
 
